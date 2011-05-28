25c3f56e381f177a80b521985c679c43d05cb72a
James Saryerwinnie <jls.npi@gmail.com>
2011-05-27 17:17:25 -0700

    Display list command is a table
    Just a rough take, there's probably a more general
    way to structure this, which will likely be obvious
    after writing a few more list commands.

--- labmanager/shell.py
+++ labmanager/shell.py
@@ -1,14 +1,43 @@
 import cmd
+from texttable import Texttable
+
+
+# A mapping from the SOAP returned names
+# to the nicer to display names.
+DISPLAY_TYPE_MAP = {
+    'dateCreated': 'created',
+    'fenceMode': 'fencemode',
+    'isDeployed': 'deployed',
+    'isPublic': 'public',
+    'bucketName': 'bucket',
+}
 
 
 class LMShell(cmd.Cmd):
+    DONT_SHOW_COLUMN = ['autoDeleteInMilliSeconds', 'autoDeleteDateTime',
+                        'description', 'mustBeFenced', 'fenceMode',
+                        'dateCreated']
+
     def __init__(self, lmapi, completekey='tab', stdin=None, stdout=None):
         cmd.Cmd.__init__(self, completekey, stdin, stdout)
         self._lmapi = lmapi
 
     def do_list(self, line):
-        configs = self._lmapi.list_library_configurations()
-        print configs
+        configs = self._lmapi.list_library_configurations()[0]
+        if not configs:
+            return
+        columns = [c for c in configs[0].__keylist__ if c not in
+                   self.DONT_SHOW_COLUMN]
+
+        table = Texttable(max_width=120)
+        table.set_deco(Texttable.HEADER | Texttable.VLINES)
+        table.set_cols_align(['l' for l in columns])
+        table.set_cols_width([6, 30, 6, 8, 4, 10, 15])
+        table.header([DISPLAY_TYPE_MAP.get(c, c) for c in columns])
+        for config in configs:
+            row = [getattr(config, col) for col in columns]
+            table.add_row(row)
+        print table.draw()
 
     def do_EOF(self, line):
         return True
