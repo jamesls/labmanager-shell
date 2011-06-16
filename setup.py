35d66a9e1c2afd79bdfad0e07206755654cd6c96
James Saryerwinnie <jls.npi@gmail.com>
2011-06-15 23:33:42 -0700

    Add various metadata to setup function

--- setup.py
+++ setup.py
@@ -1,13 +1,19 @@
+import os
 from setuptools import setup, find_packages
+
 
 setup(
     name='lmsh',
     version='0.1',
     description="Command line interface to Lab Manager SOAP API",
+    long_description=open(os.path.join(os.path.dirname(__file__),
+                                       'README.rst')).read()
     license='BSD',
     author='James Saryerwinnie',
     author_email='jlsnpi@gmail.com',
     packages=find_packages(),
+    keywords="labmanager",
+    url="https://github.com/jamesls/labmanager-shell",
     entry_points={
         'console_scripts': ['lmsh = labmanager.shell:main'],
     },
